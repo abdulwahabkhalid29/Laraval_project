21d6ed614b1e36accc268db9387d18165935320c
abdulwahabkhalid <98513650+MFahadKhalid@users.noreply.github.com>
2022-08-16 11:53:36 +1300

    laravel_project

--- secondapp/resources/views/layout/scaffold.blade.php
+++ secondapp/resources/views/layout/scaffold.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Include</title>
-    @include('layout.link')
+    <title>Document</title>
+    @include('include.style')
 </head>
 <body>
     @yield('content')
-    @include('layout.script')
+    @include('include.script')
 </body>
 </html>
